38e92dcc232dddb0b2c3fe294e79fb27a2494387
unknown <MTXDevp@hotmail.com>
2020-02-25 14:06:13 +0100

    app-test

---
+++ app-stencil/src/components/app-home/app-home.tsx
@@ -0,0 +1,25 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: true
+})
+export class AppHome {
+
+  render() {
+    return (
+      <div class='app-home'>
+        <p>
+          HOME
+        </p>
+
+        <stencil-route-link url='/profile/stencil'>
+          <button>
+            GO PROFILE PAGE
+          </button>
+        </stencil-route-link>
+      </div>
+    );
+  }
+}
